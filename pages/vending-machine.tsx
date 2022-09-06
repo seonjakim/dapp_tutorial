9500f67bdc555e99aeb25309a2241a537840e529
김선자 <seonjakim@gimseonjas-MacBook-Pro.local>
2022-09-06 17:03:55 +0900

    feat: initial setting

---
+++ pages/vending-machine.tsx
@@ -0,0 +1,18 @@
+import "bulma/css/bulma.css";
+
+import { Fragment } from "react";
+
+import Head from "next/head";
+
+const VendingMachine = () => {
+  return (
+    <Fragment>
+      <Head>
+
+      </Head>
+      <div>vending-machine</div>
+    </Fragment>
+  )
+}
+
+export default VendingMachine
